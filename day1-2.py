76be781fb63eef5f5dfc0cccc0c993eb4fd13d57
antoine.laurent <antoine.laurent@reseau.eseo.fr>
2022-12-05 22:30:57 +0100

    first commit

---
+++ day1-2.py
@@ -0,0 +1,15 @@
+if __name__ == "__main__":
+    elvesCalories = []
+    with open("inputs/day1-1.txt") as f:
+        data = f.read().splitlines()
+        current = 0
+        for index, input in enumerate(data):
+            if input == "" or index == len(data) - 1:
+                elvesCalories.append(current)
+                current = 0
+            else:
+                current += int(input)
+
+    elvesCalories.sort(reverse=True)
+    with open("outputs/day1-2.txt", "w") as f:
+        f.write(str(elvesCalories[0] + elvesCalories[1] + elvesCalories[2]))
